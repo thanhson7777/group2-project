1c40023225b40e1eb9ea52f3d58cb7bd9f2217ff
thanhson7777 <thanhson11052003@gmail.com>
2025-10-29 01:07:35 +0700

    Cai dat mongoose va cau hinh mongodb

---
+++ backend/models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
